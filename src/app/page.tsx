7a9154e804b517595b6da0c28385883307b36f76
Samin Israr Ravi <saminisrar1@gmail.com>
2024-03-13 21:42:27 +0600

    segment2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,9 @@
 import Button from "../components/Button";
 
-export default function Home(props) {
-
+export default function Home(props: any) {
   return (
     <>
       <Button></Button>
     </>
   );
 }
-
-
